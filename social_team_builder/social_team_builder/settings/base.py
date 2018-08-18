f7cb5a0259e5935f82b9a43094c1becd9a2045e5
Stuart McIntosh <s.g.mcintosh@gmail.com>
2018-08-18 21:15:51 +0930

    added static root

--- social_team_builder/social_team_builder/settings/base.py
+++ social_team_builder/social_team_builder/settings/base.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
